9b5882ed2ed96d12e01957e48bc3045c4e35bb9b
DsFBoI <98948381+DsFBoI@users.noreply.github.com>
2024-07-04 16:09:53 +0200

    Cambios para machine learning

--- src/main/java/com/example/Oracle.java
+++ src/main/java/com/example/Oracle.java
@@ -7,17 +7,27 @@ import java.util.Collection;
 
 public class Oracle implements MembershipOracle.DFAMembershipOracle<Character> {
 
-    TestDriver tD= new TestDriver();
+    
+    TestDriver tD;
+
+    public Oracle() throws Exception {
+        // Add your exception handling logic here
+        tD= new TestDriver();
+    }
 
     @Override
     public void processQueries(Collection<? extends Query<Character, Boolean>> queries) {
-        
+
         for(Query<Character, Boolean> query : queries)
         {
             tD.reset();
             boolean out = true;
             for (Character input : query.getInput()) {
-                out = tD.executeSymbol(input);
+                try {
+                    out = tD.executeSymbol(input);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             query.answer(out);
